9f13a9990026c3aeda0b4fcdb4f8f07b935790f9
@shekhar <shekhardeuba36@gmail.com>
2024-01-30 21:56:20 +0545

    routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import "./App.css";
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Contact from './Components/Contact';
+import About from './Components/About';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <>
+   <BrowserRouter>
    <Header />
-   <Home />
+<Routes>
+   <Route path= "/home" element={<Home />} />
+   <Route path= "/contact" element={<Contact />} />
+   <Route path= "/About" element={<About />} />
+</Routes>
+<Footer />
+   </BrowserRouter>
     </>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
   return (
+    <>
     <nav>Daily Goals To Complete</nav>
+    <div className="header">
+    <Link to = "/Home" >Home</Link>
+    <Link to = "/Contact" >Contact</Link>
+    <Link to = "/About" >About</Link>
+    </div>
+    </>
   )
 }
 
